21853c3961741c8eaee22b390b34510ed6f803e4
miguelilloGR <migueguardia11@gmail.com>
2023-12-02 12:45:21 +0100

    Update App.java

--- nuevoMaven/src/main/java/com/mgh/edd/App.java
+++ nuevoMaven/src/main/java/com/mgh/edd/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "En resumen, Ant es más manual y flexible, Maven se basa en convenciones y gestión de dependencias, mientras que Gradle ofrece flexibilidad y control con un lenguaje de configuración más expresivo. La elección entre ellas depende de las preferencias del equipo de desarrollo, la complejidad del proyecto y la necesidad de personalización." );
+        System.out.println( "MAVEN: es una herramienta que se utiliza para la construccions de aplicaciones, gestion de dependencias y de versiones dando soporte a todo el ciclo de desarrollo. GRADLE: es una herramienta que se ocupa de combinar las tareas y personalizar el ANT. ANT: es una biblioteca de java que nos permite automatizar el proceso de construccion de aplicaciones" );
     }
 }
